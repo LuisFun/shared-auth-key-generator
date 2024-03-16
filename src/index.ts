b1fb66bc94828575a3484e2a51a5c5e12ed64a19
Luis <101060519+LuisFun@users.noreply.github.com>
2024-03-16 14:53:31 +0900

    fix: url

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,40 @@ type Env = {
   DISCORD_WEBHOOK_URL: string
 }
 
+const sites = [
+  {
+    title: 'Paimon+',
+    url: 'https://paimon.plus',
+    description: 'Beta Access Key',
+    icon: '/maskable_icon_x128.png',
+  },
+  {
+    title: 'AI Paint',
+    url: 'https://ai-paint.luis.fun',
+    description: 'No Ads Key',
+    icon: '/favicon.png',
+  },
+]
+
 const task = async (env: Env) => {
-	// create new key
+  // create new key
   const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   const N = 16
   const key = [...Array(N)].map(() => S[Math.floor(Math.random() * S.length)]).join('')
   await env.KV.put('key', key)
-	// create message
+  // create message
   const message = {
-    content: '@here',
-    embeds: [
-      {
-        color: 1667583,
-        title: 'Paimon+ :key:',
-        url: 'https://paimon.plus/?auth=' + key,
-        description: 'Monthly Update',
-        thumbnail: {
-          url: 'https://paimon.plus/maskable_icon_x128.png',
-        },
+    embeds: sites.map(site => ({
+      color: 1667583,
+      title: site.title + ' :key:',
+      url: site.url + '/?auth=' + key,
+      description: site.description,
+      thumbnail: {
+        url: site.url + site.icon,
       },
-      {
-        color: 1667583,
-        title: 'Image Generator :key:',
-        url: 'https://image-generator.luis.fun/?auth=' + key,
-        description: 'Monthly Update',
-        thumbnail: {
-          url: 'https://image-generator.luis.fun/favicon.png',
-        },
-      },
-    ],
+    })),
   }
-	// send message
+  // send message
   await fetch(env.DISCORD_WEBHOOK_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
